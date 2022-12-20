3e10e668f60b70c31d5dd8304cc017b95c318c15
ALX <115327608+N0T-THE-F0X@users.noreply.github.com>
2022-12-20 04:52:40 +0000

    age calc

---
+++ age-calc.py
@@ -0,0 +1,14 @@
+print(r"    _        _      _   _   ")
+print(r"   //\      | |     \ \/ /  ")
+print(r"  //_\\     | |      \/ /   ")
+print(r" //___\\    | |__    / / \  ")
+print(r"//     \\   |____|  /_/ \_\ ")
+print()
+print("Simple age calc")
+print()
+
+while True:
+    x = input("Enter your birth year or age:")
+
+    print("Your age or birth year",int(x) - 2022)
+    print()
